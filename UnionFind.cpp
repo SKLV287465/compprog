c4ecb648d435d2406598ea56e1967017a8afc57b
Joshua Shim <joshua.shim2004@gmail.com>
2025-06-30 09:33:01 +1000

    changed union find algo

--- UnionFind.cpp
+++ UnionFind.cpp
@@ -2,19 +2,52 @@
 #define long long long
 using namespace std;
 
-long find(long a, vector<long>& parent) {
-    if (parent[a] == a) {
-        return a;
-    }
-    return parent[a] = find(parent[a], parent);
-}
+class UnionFind {
+    vector<long> rank, parent;
 
-// find(a) is always the new root.
-void unite(long a, long b, vector<long>& parent) {
-    if (a == b) {
-        return;
+public:
+  
+    // Constructor to initialize sets
+    UnionFind(long n) {
+        rank.resize(n, 0);
+        parent.resize(n);
+
+        // Initially, each element is in its own set
+        for (long i = 0; i < n; i++) {
+            parent[i] = i;
+        }
     }
-    a = find(a, parent);
-    b = find(b, parent);
-    parent[b] = a;
-}
+
+    // Find the representative of the set that x belongs to
+    long find(long i) {
+        long root = parent[i];
+      
+        if (parent[root] != root) {
+            return parent[i] = find(root);
+        }
+      
+        return root;
+    }
+
+    // Union of sets containing x and y, returns new root.
+    long unite(long x, long y) {
+        long xRoot = find(x);
+        long yRoot = find(y);
+
+        // If they are in the same set, no need to union
+        if (xRoot == yRoot) return;
+
+        // Union by rank
+        if (rank[xRoot] < rank[yRoot]) {
+            parent[xRoot] = yRoot;
+            return yRoot;
+        } else if (rank[yRoot] < rank[xRoot]) {
+            parent[yRoot] = xRoot;
+            return xRoot;
+        } else {
+            parent[yRoot] = xRoot;
+            rank[xRoot]++;
+            return xRoot;
+        }
+    }
+};
